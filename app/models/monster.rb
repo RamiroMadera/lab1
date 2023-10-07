def4e292336269caf4f97fcecd0c7335765d1edc
RamiroMadera <ramiro.madera@alu.ing.unlp.edu.ar>
2023-10-07 02:07:11 +0100

    Parte 2 - asociaciones entre ataque, monster y victim

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,6 @@
 class Monster < ApplicationRecord
     has_many :tweets, dependent: :destroy
+    has_many :attacks, dependent: :destroy
+    has_many :victims, through: :attacks
     validates :name, presence: true
 end
